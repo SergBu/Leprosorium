19ed33d31ae1a2f5c0983dcd5d23bb5bf8eee2b0
Serg Budnikov <sergei.budnikov@smartseeds.ru>
2024-10-14 00:04:49 +0300

    add comments

--- app.rb
+++ app.rb
@@ -9,20 +9,44 @@ def init_db
 	@db.results_as_hash = true
 end
 
+# выполняется в начале каждого запроса
+# before выполняется каждый раз при перезагрузке
+# любой страницы
 before do 
+	# инициализация БД
 	init_db
+end
+
+# configure вызывается каждый раз при конфигурации приложения:
+# когда изменился код программы И перезагрузилась страница
+configure do 
+	# инициализация БД
+	init_db
+
+
+	@db.execute 'create table if not exists Posts
+	(
+		id INTEGER PRIMARY KEY AUTOINCREMENT, 
+		created_date DATE, 
+		content TEXT
+	)'
 end
 
 get '/' do
 	erb "Hello! <a href=\"https://github.com/bootstrap-ruby/sinatra-bootstrap\">Original</a> pattern has been modified for <a href=\"http://rubyschool.us/\">Ruby School</a>"			
 end
 
+# обработчик get-запроса /new
+# (браузер получает страницу с сервера)
 get '/new' do
   erb :new
 end
 
+# обработчик post-запроса /new
+# (браузер отправляет данные на сервер)
 post '/new' do
+  # получаем переменную из post-запроса
   @content = params[:content]
-
+  # возвращаем ответ
   erb "You typed #{@content}"
 end
